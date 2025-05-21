bb4df9b41d8de2a81a916a7fe2527751078e90ad
VoznenkoB <voznenkobogdan57@gmail.com>
2025-05-21 12:40:35 +0300

    Додавання Recipe

--- 3/recipe/models.py
+++ 3/recipe/models.py
@@ -8,3 +8,15 @@ class Category(models.Model):
 
     def __iter__(self):
         return iter([self.name])
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    instructions = models.TextField()
+    ingredients = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
