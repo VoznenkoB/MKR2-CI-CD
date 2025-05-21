41a6b7d3eb1a5ff91b016633c278e437d4bb19c1
VoznenkoB <voznenkobogdan57@gmail.com>
2025-05-21 12:51:45 +0300

    Тестування Category

--- 3/recipe/tests.py
+++ 3/recipe/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 from .models import Category, Recipe
 
+class CategoryModelTest(TestCase):
+    def test_category_creation(self):
+        category = Category.objects.create(name="Desserts")
+        self.assertEqual(str(category), "Desserts")
+        self.assertIn("Desserts", list(iter(category)))
